959b94297e2e1c4455bfe46a683c25a84f135dc2
tiechui1994 <2904951429@qq.com>
2021-11-01 22:53:06 +0800

    update

--- script/tzdata.sh
+++ script/tzdata.sh
@@ -13,45 +13,40 @@ VER=2021e
 set -e
 
 rm -rf work && mkdir work
-cd work && mkdir source
+cd work && mkdir -p tzdata
 
 # download
 curl -L -O "https://www.iana.org/time-zones/repository/releases/tzcode$VER.tar.gz"
 curl -L -O "https://www.iana.org/time-zones/repository/releases/tzdata$VER.tar.gz"
-tar xf "tzcode$VER.tar.gz" -C source
-tar xf "tzdata$VER.tar.gz" -C source
-chmod -Rf a+rX,u+w,g-w,o-w source
+tar xf "tzcode$VER.tar.gz" -C tzdata
+tar xf "tzdata$VER.tar.gz" -C tzdata
+chmod -Rf a+rX,u+w,g-w,o-w tzdata
 
 # build
-cd source
+cd tzdata
 make VERSION="$VER" AWK=awk CFLAGS=-DSTD_INSPIRED "tzdata$VER-rearguard.tar.gz"
-tar xf "tzdata$VER-rearguard.tar.gz"
+tar xvf "tzdata$VER-rearguard.tar.gz"
 rm tzdata.zi
 make VERSION="$VER" AWK=awk CFLAGS=-DSTD_INSPIRED DATAFORM=rearguard tzdata.zi
 
 files="africa antarctica asia australasia europe northamerica southamerica pacificnew etcetera backward"
 mkdir -p zoneinfo/posix && mkdir -p zoneinfo/right
-zic -y ./yearistype -d zoneinfo -L /dev/null -p America/New_York "$files"
-zic -y ./yearistype -d zoneinfo/posix -L /dev/null  "$files"
-zic -y ./yearistype -d zoneinfo/right -L leapseconds  "$files"
+zic -y ./yearistype -d zoneinfo -L /dev/null -p America/New_York ${files}
+zic -y ./yearistype -d zoneinfo/posix -L /dev/null  ${files}
+zic -y ./yearistype -d zoneinfo/right -L leapseconds  ${files}
 
 # install
-install -d ../tzinfo
-cp -prd zoneinfo ../tzinfo
-intstall -p -m 644 zone.tab zone1970.tab iso3166.tab leapseconds tzdata.zi ../tzinfo/zoneinfo
+cp -prd zoneinfo ..
+install -p -m 644 zone.tab zone1970.tab iso3166.tab leapseconds tzdata.zi ../zoneinfo
 
-cd ../tzdata/zoneinfo
-rm -f ../../zoneinfo.zip
-zip -0 -r ../../zoneinfo.zip *
+cd ../zoneinfo
+rm -f ../../zoneinfo.zip && zip -0 -r ../../zoneinfo.zip *
 cd ../..
 
-
-ls -Al
-
-#go generate time/tzdata
+go generate time/tzdata
 
 echo
-if [ "$1" = "-work" ]; then
+if [[ "$1" = "-work" ]]; then
 	echo Left workspace behind in work/.
 else
 	rm -rf work
