0b6b2792d9589317d5911ef920a2080dcccd57d5
tiechui1994 <2904951429@qq.com>
2022-11-17 11:00:57 +0800

    update chown

--- setup.sh
+++ setup.sh
@@ -3,11 +3,28 @@
 TMPDIR=$(mktemp -d)
 
 CURRENT=$PWD
-
 cd $TMPDIR
+files=(
+  .github
+  .gitignore
+  .dotfiles
+  golang
+  google
+  postfix
+  script
+  README.md
+  setup.sh
+)
+
 for script in ~/.dotfiles/.dotfiles/*; do
+  echo "exec: $script"
   bash "$script"
 done
+
+for i in ${files[@]}; then
+  rm -rf ~/.dotfiles/$i
+  rm -rf ~/$i
+fi
 cd $CURRENT
 
 rm -rf $TMPDIR
