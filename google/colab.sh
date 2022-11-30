cc8363591fdc4ae47137ad006aa50b8f12a459ae
tiechui1994 <2904951429@qq.com>
2022-11-30 10:43:05 +0800

    update frp

--- google/colab.sh
+++ google/colab.sh
@@ -10,9 +10,11 @@ colab_init() {
         done
     fi
 
-    if [[ ! -x "/usr/local/bin/ngrok" ]]; then
-        wget --quiet https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz -O ngrok.tgz
-        tar xf ngrok.tgz && mv ngrok /usr/local/bin/ngrok && rm -rf ngrok.tgz
+    if [[ ! -d "/usr/local/frp" ]]; then
+        wget --quiet https://github.com/fatedier/frp/releases/download/v0.45.0/frp_0.45.0_linux_amd64.tar.gz -O frp.tgz
+        rm -rf frp && mkdir frp
+        tar xf frp.tgz -C frp --strip-components 1 && \
+        mv frp /usr/local && rm -rf frp.tgz
     fi
 }
 
@@ -48,7 +50,7 @@ EOF
     service ssh start
 }
 
-colab_ngrok_service() {
+colab_frp_service() {
     read -r -d '' conf <<-'EOF'
 #!/bin/sh
 
@@ -62,15 +64,14 @@ colab_ngrok_service() {
 ### END INIT INFO
 
 # PATH should only include /usr/* if it runs after the mountnfs.sh script
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-DESC="$DESC"
-NAME="$NAME"
-DAEMON=$DAEMON
-DAEMON_ARGS="$ARGS"
+DESC="@DESC"
+NAME="@NAME"
+DAEMON=@DAEMON
+DAEMON_ARGS="@ARGS"
 PIDFILE=/var/run/${NAME}.pid
 SCRIPTNAME=/etc/init.d/${NAME}
 
-[ -x "$DAEMON" ] || exit 0
+[ -x "${DAEMON}" ] || exit 0
 
 . /lib/init/vars.sh
 . /lib/lsb/init-functions
@@ -80,14 +81,9 @@ do_start() {
         #   0 if daemon has been started
         #   1 if daemon was already running
         #   2 if daemon could not be started
-        start-stop-daemon --start --quiet --pidfile ${PIDFILE} --exec ${DAEMON} --test > /dev/null \
-                || return 1
-        start-stop-daemon --start --quiet --pidfile ${PIDFILE} --exec ${DAEMON} --background -- \
+        start-stop-daemon --start --quiet --make-pidfile --pidfile ${PIDFILE} --exec ${DAEMON} --background -- \
                 ${DAEMON_ARGS} \
                 || return 2
-        # Add code here, if necessary, that waits for the process to be ready
-        # to handle requests from services started subsequently which depend
-        # on this one.  As a last resort, sleep for some time.
 }
 
 do_stop() {
@@ -96,39 +92,27 @@ do_stop() {
         #   1 if daemon was already stopped
         #   2 if daemon could not be stopped
         #   other if a failure occurred
-        start-stop-daemon --stop --quiet --retry=TERM/10/KILL/5 --pidfile ${PIDFILE} --name ${NAME}
+        start-stop-daemon --stop --quiet --remove-pidfile --pidfile ${PIDFILE} --retry=TERM/10/KILL/5 --name ${NAME}
         RETVAL="$?"
         [ "$RETVAL" = 2 ] && return 2
-        # Wait for children to finish too if this is a daemon that forks
-        # and if the daemon is only ever run from this initscript.
-        # If the above conditions are not satisfied then add some other code
-        # that waits for the process to drop all resources that could be
-        # needed by services started subsequently.  A last resort is to
-        # sleep for some time.
-        start-stop-daemon --stop --quiet --oknodo --retry=0/10/KILL/5 --exec ${DAEMON}
-        [ "$?" = 2 ] && return 2
-        # Many daemons don't delete their pidfiles when they exit.
-        rm -f ${PIDFILE}
-        return "$RETVAL"
 }
 
-VERBOSE="yes"
 
 case "$1" in
   start)
-        [ "$VERBOSE" != no ] && log_daemon_msg "Starting ${DESC}" "${NAME}"
+        log_daemon_msg "Starting ${DESC}" "${NAME}"
         do_start
         case "$?" in
-                0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-                2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+                0|1) log_end_msg 0 ;;
+                2)   log_end_msg 1 ;;
         esac
         ;;
   stop)
-        [ "$VERBOSE" != no ] && log_daemon_msg "Stopping ${DESC}" "${NAME}"
+        log_daemon_msg "Stopping ${DESC}" "${NAME}"
         do_stop
         case "$?" in
-                0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-                2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+                0|1) log_end_msg 0 ;;
+                2)   log_end_msg 1 ;;
         esac
         ;;
   status)
@@ -153,119 +137,53 @@ case "$1" in
         esac
         ;;
   *)
-        #echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
         echo "Usage: ${SCRIPTNAME} {start|stop|status|restart}" >&2
         exit 3
         ;;
 esac
 EOF
 
-    ngrok=${conf}
-    ngrok=${ngrok//'$NAME'/'ngrok'}
-    ngrok=${ngrok//'$DESC'/'ngrok'}
-    ngrok=${ngrok//'$DAEMON'/'/usr/local/bin/ngrok'}
-    ngrok=${ngrok//'$ARGS'/'start --config=/etc/ngrok.yml --all'}
-    printf "%s" "$ngrok" > /etc/init.d/ngrok
-    chmod a+x /etc/init.d/ngrok
+    frpc=${conf}
+    frpc=${frpc//'@NAME'/'frpc'}
+    frpc=${frpc//'@DESC'/'frpc'}
+    frpc=${frpc//'@DAEMON'/'/usr/local/frp/frpc'}
+    frpc=${frpc//'@ARGS'/'--config=/usr/local/frp/frpc.ini'}
+    printf "%s" "$frpc" > /etc/init.d/frpc
+    chmod a+x /etc/init.d/frpc
 }
 
-colab_ngrok_config() {
-    local file OPTIND
+colab_frp_config() {
+    read -r -d '' conf <<-'EOF'
+[common]
+server_addr = frp1.freefrp.net
+server_port = 7000
+token = freefrp.net
 
-    if [[ $# -eq 0 ]]; then
-        echo "file must be set"
-        return 1
-    fi
+[ssh_@NAME_@UID]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 22
+remote_port = 20022
 
-    OPTIND=1
-    while getopts f: opt ; do
-        case "$opt" in
-            f) file="$OPTARG" ;;
-        esac
-    done
-    shift $(($OPTIND - 1))
-
-
-    if [[ ! -f "$file" ]]; then
-        echo "the args (-f file) must be set and exist"
-        return 1
-    fi
-
-    log="$(grep -E -o '^log:\s*/.*' ${file})"
-    format="$(grep -E -o '^log_format:\s*json$' ${file})"
-    authtoken="$(grep -E -o '^log_format:\s*.*$' ${file})"
-    if [[ -z "$log"  || -z "$format" || -z "$authtoken" ]]; then
-        echo "log, authtoken must set, format must be json"
-        return 1
-    fi
-
-    sed -i -E 's|^log:\s*/.*$|log: /var/log/ngrok.log|' "$file"
-    cp "$file" /etc/ngrok.yml
-
-   read -d '' -r conf <<-'EOF'
-import json
-import requests
-import re
-
-keys = {}
-
-def handle(data):
-    log = json.JSONDecoder().decode(data)
-    if 'url' in log and 'obj' in log and log['obj'] == 'tunnels':
-        keys[log['name']] = log['url']
-        if log['name'] == 'ssh':
-            matched = re.findall('tcp://([^:]+?):([0-9]+)', log['url'])
-            keys[log['name']] = 'ssh root@%s -p %s' % (matched[0][0], matched[0][1])
-        body = {
-            'ttl': 28800,
-            'value': keys
-        }
-        url = 'https://jobs.quinn1994.tk/api/mongo?key=ngrok'
-        r = requests.request("POST", url, json=body, verify=True)
-        print('result:', str(r.content, 'utf-8'))
-
-
-def read(file):
-    with open(file, mode='r', buffering=4096) as fifo:
-        temp = ''
-        while True:
-            data = fifo.read()
-            if len(data) == 0:
-                break
-
-            data = str(data)
-            n = len(data)
-            begin, end = 0, len(data)
-            nums = str(data).count('}', begin, end)
-            last = data[n - 1] == '}'
-            for i in range(0, nums):
-                length = data[begin:end].index('}')
-                if i == 0 and data[0] != '{':
-                    handle(temp + data[begin:begin + length + 1])
-                    temp = ''
-                    begin += length + 1
-                    continue
-
-                handle(data[begin:begin + length + 1])
-                begin += length + 1
-
-            if not last:
-                temp = data[begin:end]
-
-try:
-    read('$log')
-except Exception as e:
-    print(e)
 EOF
-   conf=${conf//'$log'/'/var/log/ngrok.log'}
-   printf "%s" "$conf" > /usr/local/bin/upload.py
+
+    frpc=${conf}
+    frpc=${frpc//'@NAME'/'google'}
+    frpc=${frpc//'@UID'/$(date '+%s')}
+    printf "%s" "$frpc" > /usr/local/frp/frpc.ini
+}
+
+colab_frp_upload() {
+    value="ssh root@frp1.freefrp.net -p 20022"
+    data='{"ttl":28800, "value":"@value"}'
+    curl --request POST -sL \
+         --url 'https://api.quinn.eu.org/api/mongo?key=frpc'\
+         --data ${data//'@value'/$value}
+
+    echo "ssh: $value"
 }
 
 colab_start_service() {
     service $@ restart && sleep 5 && service $@ status
-}
-
-colab_ngrok_log() {
-    python3 /usr/local/bin/upload.py
 }
 
