bd288c0b98c567b0af7793c4ead2ddcd2296781a
quinn <2904951429@qq.com>
2025-04-16 16:24:33 +0800

    Update rust.sh

--- google/rust.sh
+++ google/rust.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rust.sh
-
-sh /tmp/rust.sh \
-  --profile default 
+ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.80.0 && source $HOME/.cargo/env \
+  rustup --version; \
+  cargo --version; \
+  rustc --version;
