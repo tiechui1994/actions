713ae0c9065c6a18de769093eed7e3ba74ca4b6b
quinn <2904951429@qq.com>
2025-10-20 02:35:04 +0000

    update

--- choreo/serverless/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-curl -sL https://api.quinn.eu.org/api/file/functionless -o /app/serverless.back && \
-chmod a+x /app/serverless.back && \
-cp /app/serverless.back /app/serverless
-
-/app/serverless
